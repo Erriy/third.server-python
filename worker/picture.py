05ccfd63d0614d1b4781742cd1d79f420469100d
erriy <erriy@msn.com>
2021-01-02 14:35:38 +0800

    udpate

---
+++ worker/picture.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+from PIL import Image
+
+
+def thumbnail(src, dst, size='small', quality=100):
+    '''
+        制作缩略图
+        - src：源文件路径
+        - dst：生成的缩略图路径
+        - size： 'small','middle', 'large'
+        - quality：质量
+    '''
+    if 'small' == size:
+        max_length = 100
+    elif 'middle' == size:
+        max_length = 300
+    elif 'large' == size:
+        max_length = 800
+    else:
+        raise 'size[%s]参数不正确'%(str(size))
+
+    im = Image.open(src)
+    width, height = im.size
+
+    if width >= max_length and height >= max_length:
+        wscale = max_length / width
+        hscale = max_length / height
+        scale = wscale if wscale > hscale else hscale
+        width = int(scale*width)
+        height = int(scale*height)
+
+    im.resize((width, height), Image.ANTIALIAS).save(dst, quality=quality)
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    src = '/home/erriy/test/IMG_3733.HEIC.jpg'
+    dst = 'test.jpg'
+    for i in ['small', 'middle', 'large']:
+        thumbnail(src, i+dst, i, 70)
+
+
