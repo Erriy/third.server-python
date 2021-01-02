37af4dc726208ae3849c556c227a1e1a63f8a9f6
erriy <erriy@msn.com>
2021-01-02 16:10:42 +0800

    update

--- worker/video.py
+++ worker/video.py
@@ -5,17 +5,24 @@ import picture
 
 
 def thumbnail(src, dst, frames=16, size='small', quality=100):
-    v = cv2.VideoCapture(src)
-    success, frame = v.read()
-    success, buffer = cv2.imencode('.jpg', frame)
-    picture.thumbnail(buffer, dst, size=size, quality=quality)
-
+    cap = cv2.VideoCapture(src)
+    all_frames = cap.get(7)
+    frames = frames if all_frames > frames else all_frames
+    gap = int(all_frames/frames)
+    for i in range(frames):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, i*gap)
+        is_success, frame = cap.read()
+        is_success, buffer = cv2.imencode('.jpg', frame)
+        picture.thumbnail(buffer, '%s_%d.jpg'%(dst, i), size=size, quality=quality)
 
 
 if __name__ == "__main__":
-    src = '/home/erriy/test/IMG_2724_HEVC.MOV'
-    dst = 'test.jpg'
-    thumbnail(src, dst)
+    src = '/home/erriy/test/test.mov'
+    dst = 'test'
+    import time
+    s = time()
+    thumbnail(src, dst, 22, 'large', quality=70)
+    print('耗时', time() - s)
 
 
 
