7fa92d5e34895594fcd15ee244513e31f5076811
erriy <erriy@msn.com>
2021-01-17 14:32:38 +0800

    update

--- broadcast/service.js
+++ broadcast/service.js
@@ -1,14 +1,16 @@
 let mdns = require('multicast-dns')();
 
 let port = process.env.PORT ? process.env.PORT : 80;
-let IPAdress = '';
-const interfaces = require('os').networkInterfaces();
-for(let devName in interfaces){
-    let iface = interfaces[devName];
-    for(let i=0;i<iface.length;i++){
-        let alias = iface[i];
-        if(alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal){
-            IPAdress = alias.address;
+let IPAdress = process.env.HOST ? process.env.HOST : '';
+if('' === IPAdress) {
+    const interfaces = require('os').networkInterfaces();
+    for(let devName in interfaces){
+        let iface = interfaces[devName];
+        for(let i=0;i<iface.length;i++){
+            let alias = iface[i];
+            if(alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal){
+                IPAdress = alias.address;
+            }
         }
     }
 }
